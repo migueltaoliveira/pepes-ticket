474b8e38c9d89fa08e6995b49f71ef841b63d0d6
André Couceiro <andrecouceiro6@hotmail.com>
2017-06-03 16:28:29 +0100

    LEDs blibk when sending request for ACK to user

--- hardware_ticket/ticketPrinter/libs/LEDcombo.c
+++ hardware_ticket/ticketPrinter/libs/LEDcombo.c
@@ -1,23 +1,40 @@
-
+//Blink all
 void LEDblinkALL(int times,int period){
 
 	for (int i=0;i<times;i++){
-		for(int j=0;j<5;j++) mcp.digitalWrite(Services.serviceLED[j],HIGH);
-		delay(period);
-		for(int j=0;j<5;j++) mcp.digitalWrite(Services.serviceLED[j],LOW);
-		delay(period);	
+			for(int j=0;j<5;j++) mcp.digitalWrite(Services.serviceLED[j],HIGH);
+			delay(period);
+			for(int j=0;j<5;j++) mcp.digitalWrite(Services.serviceLED[j],LOW);
+			delay(period);	
 	}
 
 }
 
-
+// Blink only one led
 void LEDblinkONE(int service,int times,int period){
-
+	
 	for (int i=0;i<times;i++){
 		mcp.digitalWrite(Services.serviceLED[service],HIGH);
 		delay(period);
 		for(int j=0;j<5;j++) mcp.digitalWrite(Services.serviceLED[service],LOW);
 		delay(period);	
+	}
+
+}
+
+// Blink only active services
+void LEDblinkACTIVE(int times,int period){
+
+	for (int i=0;i<times;i++){
+			for(int j=0;j<5;j++){
+				if(Services.activeServices[j]) mcp.digitalWrite(Services.serviceLED[j],HIGH);
+			}
+			delay(period);
+			for(int j=0;j<5;j++){
+				if(Services.activeServices[j]) mcp.digitalWrite(Services.serviceLED[j],LOW);
+			}
+			delay(period);
+
 	}
 
 }
